c1097fc0b94dd547b2166b7850e097fb98d3e921
hp_fang <hp_fang@shanchain.com>
2021-11-02 15:04:54 +0800

    add parse data type

---
+++ rlp/decode_type.go
@@ -0,0 +1,27 @@
+package rlp
+
+import (
+	"bytes"
+	"errors"
+	"github.com/probeum/go-probeum/log"
+)
+
+func ParseTypeByHead(b []byte) (byte, error) {
+	s := NewStream(bytes.NewReader(b), 0)
+	_, l, err := s.Kind()
+
+	if err != nil {
+		log.Error("test %d: Kind returned error: %v", l, err)
+		return 0, errors.New("unsupported account type")
+	}
+	hs := uint64(len(b)) - l
+
+	return b[hs : hs+1][0], nil
+}
+
+func ParseTypeByEnd(b []byte) (byte, error) {
+	if len(b) > 0 {
+		return b[len(b)-1 : len(b)][0], nil
+	}
+	return 0, errors.New("unsupported account type")
+}
