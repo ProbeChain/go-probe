d22bbe437bb2bf48c60f9dea7a96acb06c0b0e54
ww.su <ww_su@shanchain.com>
2021-11-08 16:47:56 +0800

    eth probe

---
+++ common/const.go
@@ -0,0 +1,6 @@
+package common
+
+const (
+	ETH   = "eth"
+	PROBE = "probe"
+)
--- internal/jsre/deps/web3.js
+++ internal/jsre/deps/web3.js
@@ -2549,6 +2549,7 @@ require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requ
       this._requestManager = new RequestManager(provider);
       this.currentProvider = provider;
       this.probe = new Probe(this);
+      this.eth = this.probe
       this.db = new DB(this);
       this.shh = new Shh(this);
       this.net = new Net(this);
