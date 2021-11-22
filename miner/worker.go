629686957b9e0dfe9b8115a2a5beec7508c55db3
alexSmallWorld <chenwei01118@qq.com>
2021-11-22 10:14:23 +0800

    use 1/3 in least dpos ack required

--- miner/worker.go
+++ miner/worker.go
@@ -96,8 +96,8 @@ var (
 	MostDposWitness = DposWitnessNumber*2/3 + 1
 
 	// LeastDposWitness the least number of witness to product block
-	//LeastDposWitness = DposWitnessNumber*1/3 + 1
-	LeastDposWitness = DposWitnessNumber*1/2 + 1
+	LeastDposWitness = DposWitnessNumber*1/3 + 1
+	//LeastDposWitness = DposWitnessNumber*1/2 + 1
 
 	// dposAckChanSize is the size of channel listening to DposAckEvent.
 	dposAckChanSize = DposWitnessNumber * 10
@@ -1331,7 +1331,7 @@ func calcDifficulty(time uint64, parent *types.Header) *big.Int {
 func updateDposParams(dposSize int) {
 	DposWitnessNumber = uint(dposSize)
 	MostDposWitness = DposWitnessNumber*2/3 + 1
-	//LeastDposWitness = DposWitnessNumber*1/3 + 1
-	LeastDposWitness = DposWitnessNumber*1/2 + 1
+	LeastDposWitness = DposWitnessNumber*1/3 + 1
+	//LeastDposWitness = DposWitnessNumber*1/2 + 1
 	dposAckChanSize = DposWitnessNumber * 10
 }
