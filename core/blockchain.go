bf6c433d35908e57d6e2fd80b85ecce8cb9f97eb
yuzhao <zhaoyu@probe-group.com>
2021-12-23 10:44:31 +0800

    fix  powUncle   error

--- core/blockchain.go
+++ core/blockchain.go
@@ -3118,6 +3118,10 @@ func (bc *BlockChain) GetLatestPowAnswer(number *big.Int) *types.PowAnswer {
 func (bc *BlockChain) GetUnclePowAnswers(number *big.Int) []*types.PowAnswer {
 	uncles := maxUnclePowAnswer
 	ans := make([]*types.PowAnswer, 0, uncles*2)
+	if number.Uint64() < 2 {
+		return ans
+	}
+	number = number.Sub(number, common.Big1)
 	ret := make([]*types.PowAnswer, 0, uncles*2)
 
 	var used map[common.Hash]*types.PowAnswer
